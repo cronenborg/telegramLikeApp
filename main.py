a087a8547adc90fb0e87067f71e6bad27e004af7
lightFiregame <lightFiregame3wz7apq@outlook.it>
2025-12-15 20:26:00 +0100

    provato ad aggiungere main.py, dovrei sapere anche come cancellarlo nel caso

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+a=1
+b=1
+t=0
+while(a<b*3):
+    a=int(input("dammi un numero:"))
+    b=int(input("dammi un numero:"))
+for i in range(random.randint(1,4)):
+     t=a+i
+     print("succesivo:",t)
