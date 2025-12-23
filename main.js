12b32531812d397c46454895aa68f1a6f91ec2db
lightFiregame <lightFiregame3wz7apq@outlook.it>
2025-12-23 16:36:40 +0100

    rifinito main.ts

---
+++ main.js
@@ -0,0 +1,25 @@
+//elenco membri di una classe e la loro età
+var studente1 = "Marco";
+var eta1 = 16;
+var studente2 = "Roberto";
+var eta2 = 17;
+var studente3 = "Luca";
+var eta3 = 18;
+console.log(studente1, "ha", eta1);
+console.log(studente2, "ha", eta2);
+console.log(studente3, "ha", eta3);
+var somma = eta1 + eta2;
+console.log(somma);
+var classe;
+classe = [];
+classe.push([studente1, eta1]);
+classe.push([studente2, eta2]);
+console.log(classe[0][0], "ha", classe[0][1], "anni");
+console.log(classe[1][0], "ha", classe[1][1], "anni");
+var studenteA = ["Andrea", 18];
+var studenteB = ["Ronaldo", 17];
+console.log("lo studente", studenteA[0], "ha", studenteA[1]);
+var eseguibile = [
+    ["+", 6]
+];
+console.log(eseguibile[0][0]);
